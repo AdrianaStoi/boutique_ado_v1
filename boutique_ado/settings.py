057ecee272b5b00ad3aa713dcd873ffb598e1d38
AdrianaStoi <>
2023-11-07 16:51:14 +0000

    Add AWS keys to env.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -15,8 +15,10 @@ import os
 import dj_database_url
 
 from pathlib import Path
-if os.path.isfile('env.py'):
+
+if os.path.isfile(os.path.join(os.path.dirname(__file__), 'env.py')):
     import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
